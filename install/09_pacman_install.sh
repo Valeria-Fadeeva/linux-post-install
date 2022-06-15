72f5bbfb9ff6f8a44f388ac580d38f50e5eedf67
Valeria Fadeeva <valeria.fadeeva.me@gmail.com>
2022-06-15 13:29:48 +0500

    Init

---
+++ install/09_pacman_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ "$UID" != 0 ]]; then
+    echo "USER NOT ROOT"
+    sudo $0
+    exit
+else
+    echo "USER IS ROOT"
+fi
+
+pacman -Syu
+
+pacman -S --needed --overwrite="*" $(cat pkgs.nvidia.txt)
+pacman -S --needed --overwrite="*" $(cat pkgs.base_desktop.txt)
+pacman -S --needed --overwrite="*" $(cat pkgs.note.txt)
+pacman -S --needed --overwrite="*" $(cat pkgs.python.txt)
+pacman -S --needed --overwrite="*" $(cat pkgs.js.txt)
+pacman -S --needed --overwrite="*" $(cat pkgs.php.txt)
+pacman -S --needed --overwrite="*" $(cat pkgs.virt.txt)

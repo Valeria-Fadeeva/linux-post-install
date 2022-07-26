03eae2e85325b952babb98332c609567ae12fcb5
Valeria Fadeeva <valeria.fadeeva.me@gmail.com>
2022-07-26 14:43:06 +0500

    Fix install script 00_

--- install/00_clone_linux-boot-efi.sh
+++ install/00_clone_linux-boot-efi.sh
@@ -5,15 +5,15 @@ cd ../..
 git clone https://github.com/Valeria-Fadeeva/linux-boot-efi.git
 if [[ "$?" != 0 ]]; then
     if [[ -d "linux-boot-efi" ]]; then
+        echo $(pwd)
         cd linux-boot-efi
+        echo $(pwd)
         git pull
-        bash install_all.sh
+        bash ./install_all.sh
     fi
 else
+    echo $(pwd)
     cd linux-boot-efi
-    bash install_all.sh
+    echo $(pwd)
+    bash ./install_all.sh
 fi
-
-
-
-

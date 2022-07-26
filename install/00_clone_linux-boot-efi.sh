7c77ad71393d7129f1573fb42adc06eb734fec40
Valeria Fadeeva <valeria.fadeeva.me@gmail.com>
2022-07-26 14:30:59 +0500

    Add donate button to GitHub

--- install/00_clone_linux-boot-efi.sh
+++ install/00_clone_linux-boot-efi.sh
@@ -3,7 +3,17 @@
 cd ../..
 
 git clone https://github.com/Valeria-Fadeeva/linux-boot-efi.git
+if [[ "$?" != 0 ]]; then
+    if [[ -d "linux-boot-efi" ]]; then
+        cd linux-boot-efi
+        git pull
+        bash install_all.sh
+    fi
+else
+    cd linux-boot-efi
+    bash install_all.sh
+fi
 
-cd linux-boot-efi
 
-bash install_all.sh
+
+

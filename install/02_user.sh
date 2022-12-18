d202c7c0075dd1538cf212ec0360ebfd769fa31f
Valeria Fadeeva <valeria.fadeeva.me@gmail.com>
2022-12-18 22:56:18 +0500

    Added dolphinui.rc

--- install/02_user.sh
+++ install/02_user.sh
@@ -2,6 +2,8 @@
 
 cd "$(dirname $0)"
 
+mkdir -p $HOME/.config/
 cp --verbose --recursive --force --remove-destination --no-dereference --preserve=links ./root/.config/mc $HOME/.config/
-cp --verbose --force --remove-destination --no-dereference --preserve=links ./user/.local/share/kxmlgui5/dolphin/dolphinui.rc $HOME/.local/share/kxmlgui5/dolphin/
 
+mkdir -p $HOME/.local/share/kxmlgui5/dolphin/
+cp --verbose --force --remove-destination --no-dereference --preserve=links ./user/.local/share/kxmlgui5/dolphin/dolphinui.rc $HOME/.local/share/kxmlgui5/dolphin/
